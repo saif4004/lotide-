646f517a0ec1afb6c10d4d59f77b3d94270711b0
Saif Al-saedi <saif4004@gmail.com>
2024-09-17 08:54:51 -0700

    Third assertEqual.js commit

--- assertEqual.js
+++ assertEqual.js
@@ -4,7 +4,7 @@ const assertEqual = function(actual, expected) {
   if (actual === expected) {
     console.log(`Assertion Passed:  ${actual}   ===  ${expected}`);
   } else {
-    console.log(`Assertion Failed:  ${actual}   ===  ${expected}`);
+    console.log(`Assertion Failed:  ${actual}   !==  ${expected}`);
 
   }
 };
