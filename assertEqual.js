51694f4914ca03dc2dfff5ac5aa7aecfb2b8c728
Saif Al-saedi <saif4004@gmail.com>
2024-09-16 19:08:46 -0700

    First assertEqual.js commit

---
+++ assertEqual.js
@@ -0,0 +1,17 @@
+// FUNCTION IMPLEMENTATION
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log("Assertion Passed: " + actual + " === " + expected);
+  } else {
+    console.log("Assertion Failed: " + actual + " !== " + expected);
+
+  }
+};
+
+//TEST CODE
+
+assertEqual("Lighthouse Labs","Bootcamp");
+assertEqual(1,1);
+assertEqual(1,-2);
+assertEqual(1,2);
+assertEqual("Lighthouse Labs","Lighthouse Labs");
