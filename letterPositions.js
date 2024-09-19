7b8520804163d970a502bb9eed3873521f278cfd
Saif Al-saedi <saif4004@gmail.com>
2024-09-19 09:16:18 -0700

    First letterPositions.js

---
+++ letterPositions.js
@@ -0,0 +1,43 @@
+const eqArrays = function(arr1,arr2) {
+  if (arr1.length !== arr2.length) {
+    return false;
+  }
+  let tempLength = arr1.length;
+  if (arr2.length > tempLength) {
+    tempLength = arr2.length;
+  }
+  for (let i = 0; i < tempLength; i++) {
+    if (arr1[i] !== arr2[i]) {
+      return false;
+    }
+  }
+  return true;
+};
+
+const assertArraysEqual = function(arr1,arr2) {
+  
+  if (eqArrays(arr1,arr2)) {
+    console.log("✅✅✅ Assertion Passed: Arrays are equal");
+  } else {
+    console.log("🛑🛑🛑 Assertion Failed: Arrays are not equal");
+  }
+};
+
+const letterPositions = function(sentence) {
+  const results = {};
+  for (let i = 0; i < sentence.length; i++) {
+    const char = sentence[i];
+
+    if (char !== ' ') {
+      if (!results[char]) {
+        results[char] = [];
+      }
+      results[char].push(i);
+    }
+  }
+  return results;
+};
+
+console.log(letterPositions("hello"));
+
+assertArraysEqual(letterPositions("hello").e, [1]);
