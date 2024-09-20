eaffdf8e71cea20ae977ab6c05457a42bc9a8c68
Saif Al-saedi <saif4004@gmail.com>
2024-09-20 09:24:07 -0700

    First find key commit

---
+++ findKey.js
@@ -0,0 +1,31 @@
+const assertEqual = function(actual, expected) {
+
+  if (actual === expected) {
+    console.log(`✅✅✅ Assertion Passed:  ${actual}   ===  ${expected}`);
+  } else {
+    console.log(`🛑🛑🛑 Assertion Failed:  ${actual}   !==  ${expected}`);
+
+  }
+};
+
+const findKey = function(obj1,callback) {
+
+  for (const key in obj1) {
+    if(callback(obj1[key]) === true) {
+      return key;
+    }
+  }
+};
+
+
+findKey(
+  {
+    "Blue Hill": { stars: 1 },
+    Akaleri: { stars: 3 },
+    noma: { stars: 2 },
+    elBulli: { stars: 3 },
+    Ora: { stars: 2 },
+    Akelarre: { stars: 3 },
+  },
+  (x) => x.stars === 2
+); // => "noma"
