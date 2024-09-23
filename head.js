10222fdfe47da872b9b4a9b9bc8113488755a227
Saif Al-saedi <saif4004@gmail.com>
2024-09-23 08:19:03 -0700

    Removed the test cases and exported the head function

--- head.js
+++ head.js
@@ -1,6 +1,4 @@
 // FUNCTION IMPLEMENTATION
-
-
 const head = function(arr) {
   if (arr.length <= 0) {
 
@@ -8,21 +6,6 @@ const head = function(arr) {
   }
   return arr[0];
 };
-const assertEqual = function(actual, expected) {
-  if (actual === expected) {
-    console.log(`Assertion Passed:  ${actual}   ===  ${expected}`);
-  } else {
-    console.log(`Assertion Failed:  ${actual}   !==  ${expected}`);
-  }
-};
 
-//TEST CODE
+module.exports = head;
 
-assertEqual(head([5, 6, 7]), 5);
-assertEqual(head(["Hello", "Lighthouse", "Labs"]), "Hello");
-assertEqual(head([5, 6, 7]), 7);
-assertEqual(head(["Hello", "Lighthouse", "Labs"]), "Eric");
-assertEqual(head([5, 6, 7]), 5);
-assertEqual(head(["Hello", "Lighthouse", "Labs"]), "Garry");
-assertEqual(head([5, 6, 7]), 5);
-assertEqual(head(["Hello", "Lighthouse", "Labs"]), "Zack");
