c2a5d4e857b5668a006a9970535cb02f8cdb8099
Saif Al-saedi <saif4004@gmail.com>
2024-09-23 09:05:30 -0700

    Fixed some tests case to test arrays proproperly

--- test/tailTest.js
+++ test/tailTest.js
@@ -3,7 +3,6 @@ const tail = require("../tail");
 //TEST CODE
 
 const result = tail(["Hello", "Lighthouse", "Labs"]);
-console.log(result);
 assertEqual(result.length, 2);
 const words = tail(["Yo Yo", "Lighthouse", "Labs"]);
 assertEqual(words.length, 2); 
