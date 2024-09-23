4e6de7901c7a682c0f11ba02848559647156dfdf
Saif Al-saedi <saif4004@gmail.com>
2024-09-23 08:16:04 -0700

    Added an assertEqualTest test file

---
+++ test/assertEqualTest.js
@@ -0,0 +1,7 @@
+const assertEqual = require("../assertEqual");
+
+assertEqual("Lighthouse Labs","Bootcamp");
+assertEqual(1,1);
+assertEqual(1,-2);
+assertEqual(1,2);
+assertEqual("Lighthouse Labs","Lighthouse Labs");
