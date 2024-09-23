6afdc17649b1acc308ac2ec489d30491f6b620fa
Saif Al-saedi <saif4004@gmail.com>
2024-09-23 08:20:00 -0700

    Removed the test cases and exported assertEqual function

--- assertEqual.js
+++ assertEqual.js
@@ -9,10 +9,4 @@ const assertEqual = function(actual, expected) {
   }
 };
 
-//TEST CODE
-
-assertEqual("Lighthouse Labs","Bootcamp");
-assertEqual(1,1);
-assertEqual(1,-2);
-assertEqual(1,2);
-assertEqual("Lighthouse Labs","Lighthouse Labs");
+module.exports = assertEqual;
