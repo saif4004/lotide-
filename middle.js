b7b0d539df62e20b1be7e7ecb0b3de5d46d49ff4
Saif Al-saedi <saif4004@gmail.com>
2024-09-23 12:28:05 -0700

    Moved the test cases to middleTest.js file

--- middle.js
+++ middle.js
@@ -1,32 +1,3 @@
-const eqArrays = function(arr1,arr2) {
-  if (!Array.isArray(arr1) || !Array.isArray(arr2)) {
-    return false;
-  }
-  if (arr1.length !== arr2.length) {
-    return false;
-  }
-  let tempLength = arr1.length;
-  if (arr2.length > tempLength) {
-    tempLength = arr2.length;
-  }
-
-  for (let i = 0; i < tempLength; i++) {
-    if (arr1[i] !== arr2[i]) {
-      return false;
-    }
-  }
-  return true;
-};
-
-const assertArraysEqual = function(arr1,arr2) {
-  
-  if (eqArrays(arr1,arr2)) {
-    console.log(`Assertion Passed:  ${arr1}   ===  ${arr2}`);
-  } else {
-    console.log(`Assertion Failed:  ${arr1}   !==  ${arr2}`);
-  }
-};
-
 const middle = function(arr) {
   if (!Array.isArray(arr)) {
     return [];
@@ -45,14 +16,4 @@ const middle = function(arr) {
   }
 };
 
-console.log(middle([1])); // => []
-console.log(middle([1, 2])); // => []
-console.log(middle([1, 2, 3]));// => [2]
-console.log(middle([1, 2, 3, 4, 5])); // => [3]
-console.log(middle([1, 2, 3, 4])); // => [2, 3]
-console.log(middle([1, 2, 3, 4, 5, 6]));// => [3, 4]
-
-const numArray = [1,2,3,43,5,6];
-middle(numArray);
-
-assertArraysEqual(numArray,[1,2,3,43,5,6]);
+module.exports = middle;
