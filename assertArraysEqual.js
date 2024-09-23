a2c5337539302fd333042896f0b0f01fed5188e1
Saif Al-saedi <saif4004@gmail.com>
2024-09-23 09:46:23 -0700

    Removed the eqArrays function and insted I required it

--- assertArraysEqual.js
+++ assertArraysEqual.js
@@ -1,33 +1,12 @@
-const eqArrays = function(arr1,arr2) {
-  if (!Array.isArray(arr1) || !Array.isArray(arr2)) {
-    return false;
-  }
-  if (arr1.length !== arr2.length) {
-    return false;
-  }
-  let tempLength = arr1.length;
-  if (arr2.length > tempLength) {
-    tempLength = arr2.length;
-  }
-
-  for (let i = 0; i < tempLength; i++) {
-    if (arr1[i] !== arr2[i]) {
-      return false;
-    }
-  }
-  return true;
-};
+const eqArrays = require("./eqArrays.js");
 
 const assertArraysEqual = function(arr1,arr2) {
   
   if (eqArrays(arr1,arr2)) {
-    console.log(`Assertion Passed:  ${arr1}   ===  ${arr2}`);
+    console.log(`✅✅✅ Assertion Passed: ${arr1} === ${arr2}`);
   } else {
-    console.log(`Assertion Failed:  ${arr1}   !==  ${arr2}`);
+    console.log(`🛑🛑🛑 Assertion Failed: ${arr1} !== ${arr2}`);
   }
 };
 
-let arr1 = [1,2,3,4];
-let arr2 = [3,4,1,2];
-
-assertArraysEqual(arr1,arr2);
+module.exports = assertArraysEqual;
