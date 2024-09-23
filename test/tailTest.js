37938477730403bff0b66b225d26db5cd984d632
Saif Al-saedi <saif4004@gmail.com>
2024-09-23 08:57:46 -0700

    Fixed a test case to test arrays proproperly

--- test/tailTest.js
+++ test/tailTest.js
@@ -2,14 +2,11 @@ const assertEqual = require("../assertEqual");
 const tail = require("../tail");
 //TEST CODE
 
-const result = tail(["Hello", "Lighthouse", "Labs"]); // should return ["Lighthouse", "Labs"]
-assertEqual(result, ["Lighthouse", "Labs"]); // => will always fail!
-
-// Test Case: Check the original array
+const result = tail(["Hello", "Lighthouse", "Labs"]);
+assertEqual(result.length, 2);
 const words = ["Yo Yo", "Lighthouse", "Labs"];
-tail(words); // no need to capture the return value since we are not checking it
-assertEqual(words.length, 3); // original array should still have 3 elements!
-
+tail(words); 
+assertEqual(words.length, 3); 
 const words1 = ["Yo Yo"];
 tail(words1);
 assertEqual(words1.length, 1); 
