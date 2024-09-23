6ff38b3b873a2f4ebe6425b6d519678065575aae
Saif Al-saedi <saif4004@gmail.com>
2024-09-23 08:31:52 -0700

    Moved the test cases to testTail.js

--- tail.js
+++ tail.js
@@ -1,6 +1,5 @@
 // FUNCTION IMPLEMENTATION
 
-
 const tail = function(arr) {
   let tempArry = [];
   if (!Array.isArray(arr)) {
@@ -16,27 +15,5 @@ const tail = function(arr) {
  
   return tempArry;
 };
-const assertEqual = function(actual, expected) {
-  if (actual === expected) {
-    console.log(`Assertion Passed:  ${actual}   ===  ${expected}`);
-  } else {
-    console.log(`Assertion Failed:  ${actual}   !==  ${expected}`);
-  }
-};
 
-//TEST CODE
-
-const result = tail(["Hello", "Lighthouse", "Labs"]); // should return ["Lighthouse", "Labs"]
-assertEqual(result, ["Lighthouse", "Labs"]); // => will always fail!
-
-// Test Case: Check the original array
-const words = ["Yo Yo", "Lighthouse", "Labs"];
-tail(words); // no need to capture the return value since we are not checking it
-assertEqual(words.length, 3); // original array should still have 3 elements!
-
-const words1 = ["Yo Yo"];
-tail(words1);
-assertEqual(words1.length, 1); 
-const words2 = [];
-tail(words2);
-assertEqual(words2.length, 0); 
+module.exports = tail;
